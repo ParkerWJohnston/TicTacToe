afd8c04f85f925e169521a0cd20c928db92bac16
ParkerWJohnston <parkerwjohnston@gmail.com>
2018-11-23 17:29:21 -0500

    Added Functionality to prevent overwriting boardPlaces

--- TicTacToe/TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe/TicTacToe.cpp
@@ -2,8 +2,6 @@
 #include <iostream>
 
 using namespace std;
-int location; //location of X or O
-char turn = 'x';
 
 void printBoard(char board[9]) {
 	std::cout << " " << board[0] << " | " << board[1] << " | " << board[2] << " \n";
@@ -11,31 +9,57 @@ void printBoard(char board[9]) {
 	std::cout << " " << board[3] << " | " << board[4] << " | " << board[5] << " \n";
 	std::cout << " --|---|--\n";
 	std::cout << " " << board[6] << " | " << board[7] << " | " << board[8] << " \n";
-};
+}
 
-void turnX();
+int turnX();
+int turnO();
 
 int main()
 {
 	char gameState = 0;
-	char boardPlaces[9] = { '1','2','3','4','5','6','7','8','9' };
+	char boardPlaces[9] = { '0', '1','2','3','4','5','6','7','8' };
+	char NewPlace;
+	int location;
+	int turn = 0;
 
 	while (gameState == 0) {
 		printBoard(boardPlaces);
-		if (turn == 'x') {
-			turnX();
+		while (turn == 0) {
+			location = turnX();
+			if (boardPlaces[location] != 'X' && boardPlaces[location] != 'O') {
+				boardPlaces[location] = 'X';
+				turn = 1;
+			}
+			else {
+				std::cout << "Location already chosen, please retry\n";
+			}
 		}
-		else {
-			std::cout << "Player O, choose a location\n";
-			std::cin >> location;
-			boardPlaces[location - 1] = 'O';
-			turn = 'x';
+		printBoard(boardPlaces);
+		while(turn == 1) {
+			location = turnO();
+			if (boardPlaces[location] != 'X' && boardPlaces[location] != 'O') {
+				boardPlaces[location] = 'O';
+				turn = 0;
+			}
+			else {
+				std::cout << "Location already chosen, please retry\n";
+			}
 		}
 	}
+
+
+
 	return 0;
 }
-void turnX() {
-		std::cout << "Player X, choose a location\n";
-		std::cin >> location;
-		turn = 'o';
+int turnX() {
+	int location;
+	std::cout << "Player X, choose a location\n";
+	std::cin >> location;
+	return location;
+}
+int turnO() {
+	int location;
+	std::cout << "Player O, choose a location\n";
+	std::cin >> location;
+	return location;
 }
