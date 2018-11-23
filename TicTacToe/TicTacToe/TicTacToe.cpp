a07f32d197d7fdd8092cfc882da2d092f217b0e1
ParkerWJohnston <parkerwjohnston@gmail.com>
2018-11-23 14:59:45 -0500

    Update TicTacToe.cpp

--- TicTacToe/TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe/TicTacToe.cpp
@@ -6,25 +6,27 @@
 
 using namespace std;
 
+void getBoard(char board1, char board2, char board3, char board4, char board5, char board6, char board7, char board8, char board9) {
+	std::cout << " " << board1 << " | " << board2 << " | " << board3 << " \n";
+	std::cout << " --|---|--\n";
+	std::cout << " " << board4 << " | " << board5 << " | " << board6 << " \n";
+	std::cout << " --|---|--\n";
+	std::cout << " " << board7 << " | " << board8 << " | " << board9 << " \n";
+};
+
 int main()
 {
-    char board1 = '1';
-    char board2 = '2';
-    char board3 = '3';
-    char board4 = '4';
-    char board5 = '5';
-    char board6 = '6';
-    char board7 = '7';
-    char board8 = '8';
-    char board9 = '9';
-	void getBoard() {
-		std::cout << " " << board1 << " | " << board2 << " | " << board3 << " \n";
-		std::cout << " --|---|--\n";
-		std::cout << " " << board4 << " | " << board5 << " | " << board6 << " \n";
-		std::cout << " --|---|--\n";
-		std::cout << " " << board7 << " | " << board8 << " | " << board9 << " \n";
-	}
-	getBoard();
+    char boardPlace1 = '1';
+    char boardPlace2 = '2';
+    char boardPlace3 = '3';
+    char boardPlace4 = '4';
+    char boardPlace5 = '5';
+    char boardPlace6 = '6';
+    char boardPlace7 = '7';
+    char boardPlace8 = '8';
+    char boardPlace9 = '9';
+	getBoard(boardPlace1, boardPlace2, boardPlace3, boardPlace4, boardPlace5, boardPlace6, boardPlace7, boardPlace8, boardPlace9);
+
 	return 0;
 }
 
