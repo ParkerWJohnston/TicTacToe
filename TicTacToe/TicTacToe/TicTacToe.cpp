d29da5492ea3dd445e9a99e83b97800c8b50e141
ParkerWJohnston <parkerwjohnston@gmail.com>
2018-11-23 20:29:53 -0500

    Update TicTacToe.cpp

--- TicTacToe/TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe/TicTacToe.cpp
@@ -4,7 +4,22 @@
 #include "pch.h"
 #include <iostream>
 
-
+void printBoard() {
+    char board1 = '1';
+    char board2 = '2';
+    char board3 = '3';
+    char board4 = '4';
+    char board5 = '5';
+    char board6 = '6';
+    char board7 = '7';
+    char board8 = '8';
+    char board9 = '9';
+    std::cout << " "<< board1 <<" | "<< board2 <<" | "<< board3 <<" \n";
+    std::cout << " --|---|--\n";
+    std::cout << " "<< board4 <<" | "<< board5 <<" | "<< board6 <<" \n";
+    std::cout << " --|---|--\n";
+    std::cout << " "<< board7 <<" | "<< board8 <<" | "<< board9 <<" \n";
+}
 int main()
 {
     char board1 = '1';
